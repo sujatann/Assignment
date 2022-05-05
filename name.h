fd9af9efb3944ac541291ec9d18ebcc2b6ac19c4
sujatann <46914341+sujatann@users.noreply.github.com>
2022-05-05 13:12:04 +0530

    Create name.h

---
+++ name.h
@@ -0,0 +1,23 @@
+/**
+ * @file name.h
+ * @author Sujata Naikar(naikarsujata565.com)
+ * @brief : To find Sum of Characters in a given name
+ * @version 0.1
+ * @date 2022-05-05
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+//header gaurd
+#ifndef NAME_H_
+#define NAME_H_
+
+#include<stdio.h>
+#include"stdint.h"
+
+union char_str{
+   uint8_t integer;
+   char ;
+   
+};
+ #endif 
